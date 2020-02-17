ad489c6f405cacb95581a8f33f403f155e1ac23e
Abby Hudson <abbyeleanorhudson@gmail.com>
2020-02-17 20:26:54 +0100

    add method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,10 +11,20 @@ class GamesController < ApplicationController
     @word = params[:word]
     url = "https://wagon-dictionary.herokuapp.com/#{@word}"
     response = JSON.parse(open(url).read)
-        if response['found']
-          @message = "Congratulations! #{@word} is an english word"
-        else @message = "Sorry, #{@word} is not an english word"
-      end
+        if countdown(@word)
+          @message = "words not included"
+          if response['found']
+            @message = "Congratulations! #{@word} is an english word"
+          else @message = "Sorry, #{@word} is not an english word"
+          end
+        else
+          @message = "Sorry not included"
+        end
+    end
 
+  def countdown?(word)
+    @letters = params[:letters].split(//).sort
+    user_letters = word.upcase.split(//).sort
+    (user_letters - @letters).empty?
   end
 end
